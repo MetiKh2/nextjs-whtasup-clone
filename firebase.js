4b03ee3930813c91a3219af41d838d2765b90f03
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-05-19 17:19:24 +0430

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,7 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import {getFirestore} from 'firebase/firestore'
 import {getAuth, GoogleAuthProvider} from "firebase/auth";
 const firebaseConfig = {
-
+  apiKey: "AIzaSyCClnCHdHBDVD5frnfu7ukRVvZmRA9FdRM",
+  authDomain: "whatsup-clone-dfd37.firebaseapp.com",
+  projectId: "whatsup-clone-dfd37",
+  storageBucket: "whatsup-clone-dfd37.appspot.com",
+  messagingSenderId: "910382088451",
+  appId: "1:910382088451:web:d165518abcaa9b3b272936",
+  measurementId: "G-NKEC7Q39N6"
 };
 const app = initializeApp(firebaseConfig);
 //const analytics = getAnalytics(app);
